bcb60a939e1ec3ee95923d44a94d8c4cce209d41
Ben Zimny <bjp.zimny@gmail.com>
2022-11-12 15:17:06 +0300

    WIP gm panel, localisations

--- scripts/lib/PKControl.js
+++ scripts/lib/PKControl.js
@@ -9,15 +9,19 @@ class PKControl extends FormApplication {
         options.id = 'pk-control';
         options.template = 'modules/persistent-knowledge/templates/control.html';
         options.title = 'Persistent Knowledge - GM Controls';
-        options.width = 400;
-        options.height = 400;
+        options.width = 250;
+        options.height = 600;
         options.resizable = true;
         return options;
     }
 
     async getData() {
         // Return data to the template
-        return {};
+        return {
+            identifiables: game.actors.filter(a => a.constructor.name === 'ActorNPCPF'),
+            characters: game.actors.filter(a => a.constructor.name === 'ActorCharacterPF'),
+            settings: game.settings.get('persistent-knowledge', 'pkSettings')
+        };
     }
 
     render(force, options = {}) {
@@ -26,7 +30,26 @@ class PKControl extends FormApplication {
 
     activateListeners(html) {
         super.activateListeners(html);
-        //create events based on html template e.g.
-        //html.find('button').click(event => this.someFunction(event));
+        this.element.find('.pk-control-input').keyup(event => this.updateFilter(event));
+        this.element.find('.pk-control-result').click(event => this.select(event));
+    }
+
+    select(event) {
+        this.element.find('.pk-control-result').removeClass('selected');
+        const selectedElement = event.target.closest('.pk-control-result');
+        selectedElement.classList.add('selected');
+        this.element.find('.pk-control-detail').addClass('hidden');
+        this.element.find('.pk-control-detail').filter((_, e) => e.dataset.id === selectedElement.dataset.id).removeClass('hidden');
+
+    }
+
+    updateFilter(event) {
+        this.element.find('.pk-control-result').each((_, e) => {
+            if (~e.innerText.toLowerCase().indexOf(event.target.value.toLowerCase())) {
+                e.classList.remove('hidden');
+            } else {
+                e.classList.add('hidden');
+            }
+        });
     }
 }
